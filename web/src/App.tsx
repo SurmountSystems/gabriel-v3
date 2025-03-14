3b2de082184ed0ab6cea8e86db57cd0d34949685
jbride <jbride2001@yahoo.com>
2025-03-13 21:51:06 -0600

    improving block streaming chart

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import P2PKBlocksGraph from './components/P2PKBlocksGraph';
-import BlockStream from './components/BlockStream';
+import BlockVisualizer from './components/BlockVisualizer';
 import './App.css';
 
 const queryClient = new QueryClient();
@@ -42,7 +42,7 @@ function App() {
                 }}
               >
                 <P2PKBlocksGraph />
-                <BlockStream />
+                <BlockVisualizer />
               </div>
             } />
           </Routes>
--- web/src/config/api.ts
+++ web/src/config/api.ts
@@ -1,9 +1,16 @@
+// Determine the base URL based on the environment
+const API_BASE_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3000' // Development server URL
+  : ''; // Empty string for production to use relative paths
+
+console.log("API_BASE_URL: ", API_BASE_URL)
+
 // You could also add other API-related configuration here
 export const API_ENDPOINTS = {
-    latestBlocks: '/api/blocks/latest',
-    blockByHash: (hash: string) => `/api/block/hash/${hash}`,
-    blockByHeight: (height: number) => `/api/block/height/${height}`,
-    blockStream: '/api/blocks/stream',
+    latestBlocks: `${API_BASE_URL}/api/blocks/latest`,
+    blockByHash: (hash: string) => `${API_BASE_URL}/api/block/hash/${hash}`,
+    blockByHeight: (height: number) => `${API_BASE_URL}/api/block/height/${height}`,
+    blockStream: `${API_BASE_URL}/api/blocks/stream`,
 };
 
 
